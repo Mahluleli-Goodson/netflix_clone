f7d5d1406f927abcc571cdab221bfb8d9da2afc0
Mahluleli Goodson <mahluleli.goodson@double-eye.com>
2021-10-13 15:39:51 +0200

    add a clone badge to logo

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,13 +1,15 @@
+import "./styles.scss";
 import {FC} from "react";
 
 const Logo: FC = (): JSX.Element => {
     return (
-        <div className="flex flex-grow">
+        <div className="flex flex-grow items-center">
             <img
                 className="w-28"
                 src="/assets/images/netflix_logo.png"
                 alt="netflix clone"
             />
+            <span className="nc-logo__clone-badge">concept / clone</span>
         </div>
     );
 };
